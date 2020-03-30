4a919a05cc1703b347b8e0a80ed9b15e13690583
BenOBrien <bellarminebenobrien@hotmail.com>
2020-03-30 16:19:28 +0100

    MYsql line

--- src/main/java/com/universityoflimerick/sdaa/BackendCryptoLoot/Controller.java
+++ src/main/java/com/universityoflimerick/sdaa/BackendCryptoLoot/Controller.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.Map;
 
--- src/main/java/com/universityoflimerick/sdaa/BackendCryptoLoot/SecurityConfig.java
+++ src/main/java/com/universityoflimerick/sdaa/BackendCryptoLoot/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilterBefore(new LogFilter(), JWTAuthorizationFilter.class)
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
