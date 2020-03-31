e1a9e37d2a7c64049039d48d4561d223bc36f768
Brian <16190319@studentmail.ul.ie>
2020-03-31 13:42:02 -0700

    null check, refactored packages

--- src/main/java/com/universityoflimerick/sdaa/BackendCryptoLoot/Repositories/UserProfileRepository.java
+++ src/main/java/com/universityoflimerick/sdaa/BackendCryptoLoot/Repositories/UserProfileRepository.java
@@ -9,12 +9,6 @@ import java.util.Optional;
 
 public interface UserProfileRepository extends CrudRepository<UserProfile, Integer> {
 
-
-//    @Query(value = "SELECT name FROM `userprofiledetails` WHERE sub = :sub",
-//            nativeQuery = true)
-//    String getUserProfileBySub(@Param("sub") String sub);
-
-
     @Query(value = "SELECT * FROM `userprofiledetails` WHERE sub = :sub",
             nativeQuery = true)
     Optional<UserProfile> findBySub(@Param("sub") String sub);
